62c9c31339495b4a645d42e52f649ff0ac1e0548
SonamHarry <130358279+SonamHarry@users.noreply.github.com>
2023-11-01 17:01:53 +0530

    Add files via upload

--- Button.jsx
+++ Button.jsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
 import React from "react";
-import Modalsample from "./Modalsample";
 
 function Button({ abc, buttonName }) {
     return (
         <div>
-            <button
-                onKeyDown={(e) => {
-                    if (e.key === "Escape") {
-                        abc();
-                    }
-                }}
-                onClick={abc}
-            >
-                {buttonName}
-            </button>
+            <button onClick={abc}>{buttonName}</button>
         </div>
     );
 }
